415742ac519f667cb86165e34284988ed5eb7877
Edmar Landbrug <edmarlandbrug@gmail.com>
2026-05-05 01:55:07 +0200

    Day6.py

---
+++ Day6.py
@@ -0,0 +1,20 @@
+# functions are usually a command with () at the end of it
+# examples are like the len() and print() which are very common
+# Making your own function --> use def <function_name>(): and indent the actions that need to be carried out inside ur own function
+# After defining your function it you have to have to call your function 
+
+# While loop analogy --> vaccuum cleaner stops running until plug is removed from the socket
+# While condition/variable < > = value
+    # carry out this action
+    # do sum to the condition/variable
+
+def My_function():
+    print(5)
+# conmparison operators recap:
+# == is equal, != is not equal, <= less than or equal to, >= greater than or equal to, >< greater than or less than
+
+# for loops are handy to work with lists or a range so you can pre-define to work with 
+# while loops are handy to carry out an action until the condition is met (could go infinitely)
+
+
+# COME BACK TO THIS DAY TO DEBUG THE MAZE PROBLEM MAYBE
